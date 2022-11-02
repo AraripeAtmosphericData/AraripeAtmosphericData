741552863938f738717121c1f45dcd1d58b321f3
AraripeAtmosphericData <116410172+AraripeAtmosphericData@users.noreply.github.com>
2022-11-02 14:46:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,15 +12,109 @@ const batteryHtml= document.getElementById("6")
 const altHtml = document.getElementById("7")
 const vocHtml = document.getElementById("9")
 const co2Html = document.getElementById("8")
+const btnInfo = document.getElementById("btn-info")
 const timeout = setTimeout(search, 6000)
 clearTimeout(timeout)
 let id
+let x
 
 let dadosAtuais 
 let dadoAnterior 
 
 
+console.log(screen.width)
+console.log(screen.height)
+
+if (screen.width <768) {
+    btnInfo.addEventListener('mouseenter', () => {
+        showGuide(2)
+      });
+      btnInfo.addEventListener('mouseleave', () => {
+        closeGuide(2)
+      });
+    btnInfo.style.width ="9vh"
+    btnInfo.style.height ="6vh"
+
+} else {
+    btnInfo.addEventListener('mouseenter', () => {
+        showGuide(1)
+    });
+    btnInfo.addEventListener('mouseleave', () => {
+        closeGuide(1)
+    });
+}
+
+function showGuide(mode) {
+    if(mode == 1) {
+        btnInfo.innerHTML = ""
+        btnInfo.style.width ="30vh"
+        btnInfo.style.height ="36vh"
+        btnInfo.addEventListener('transitionend', () => {
+            btnInfo.innerHTML = "preencha o campo em branco com o codigo de indentificação e clique em 'Iniciar Busca' para visualizar seus dados"
+          });
+        } 
+        if(mode == 2) {
+            btnInfo.innerHTML = ""
+            btnInfo.style.width ="12vh"
+            btnInfo.style.height ="20vh"
+            btnInfo.addEventListener('transitionend', () => {
+                btnInfo.innerHTML = "preencha o campo em branco com o codigo de indentificação e clique em 'Iniciar Busca' para visualizar seus dados"
+              });
+            } 
+   
+
+}
+function closeGuide(mode) {
+    if(mode == 1) {
+    btnInfo.innerHTML = ""
+    btnInfo.style.width ="26vh"
+    btnInfo.style.height ="12vh"
+    btnInfo.addEventListener('transitionend', () => {
+        btnInfo.innerHTML = "Guia de Uso"
+      });
+    } 
+    if(mode == 2) {
+        btnInfo.innerHTML = ""
+        btnInfo.style.width ="9vh"
+        btnInfo.style.height ="5vh"
+        btnInfo.addEventListener('transitionend', () => {
+            btnInfo.innerHTML = "Guia de Uso"
+          });
+        } 
+}
+function visibilidade(type) {
+    if(type == 0) {
+        pressaoHtml.classList.add("transparent")
+        bateriaHtml.classList.add("transparent")
+        payloadAltitudeHtml.classList.add("transparent")
+        payloadCo2Html.classList.add("transparent")
+        payloadVocHtml.classList.add("transparent")
+    } else if (type == 1) {
+        pressaoHtml.classList.remove("transparent")
+        bateriaHtml.classList.remove("transparent")
+        payloadAltitudeHtml.classList.remove("transparent")
+        payloadCo2Html.classList.remove("transparent")
+        payloadVocHtml.classList.remove("transparent")
+    } else if (type == 2) {
+        pressHtml.classList.remove("transparent")
+        batteryHtml.classList.remove("transparent")
+        altHtml.classList.remove("transparent")
+        co2Html.classList.remove("transparent")
+        vocHtml.classList.remove("transparent")
+    } else if (type == 3) {
+        pressHtml.classList.add("transparent")
+        batteryHtml.classList.add("transparent")
+        altHtml.classList.add("transparent")
+        co2Html.classList.add("transparent")
+        vocHtml.classList.add("transparent")
+    }
+}
+visibilidade(0)
+visibilidade(3)
+
+
 function comparar() {
+
         let tempDif = dadosAtuais.temperatura - dadoAnterior.temperatura 
     
         if(dadoAnterior.temperatura < dadosAtuais.temperatura) {
@@ -59,6 +153,11 @@ function comparar() {
         } else {
             co2Html.innerHTML = "CO2: "+co2Dif.toFixed(3)+" ppm"
         }
+        
+        if (x = undefined) {
+            setTimeout(visibilidade(2),400)
+            x=1
+        }
 }
 function search() {
     getData()
@@ -93,6 +192,9 @@ function getData() {
     temperaturaHtml.innerHTML= "Temperatura = "+data[0].temperatura.toFixed(3)+" °C"
     pressaoHtml.innerHTML= "Pressao = "+(data[0].pressao/1000).toFixed(3)+" Kpa"
     bateriaHtml.innerHTML= "Bateria= "+((data[0].bateria/2400)*100).toFixed(2)+"%"
+    if(id==undefined) {
+        setTimeout(visibilidade(1),400)
+    }
     id = data[0]._id
     dadosAtuais = {
         temperatura:data[0].temperatura.toFixed(3),
