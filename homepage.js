ae5c1309ff8fa7f365111fef0221faf1cb7329cc
AraripeAtmosphericData <a2databaseararipeatmospheric@gmail.com>
2023-03-19 20:07:09 -0300

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,10 +2,26 @@ const divHome = document.getElementById("div-home")
 const divImg = document.getElementById("div-img")
 const divAbout = document.getElementById("div-about")
 const paragrafoInicio = document.getElementById("p1")
+const paragrafoSobre = document.getElementById("p2")
+const paragrafoImagens = document.getElementById("p3")
 
-if (screen.width <= 400) {
+if (screen.width <= 320) {
     //celular pequeno
-    paragrafoInicio.style.fontSize = "1.5vh"
+    paragrafoInicio.style.fontSize = "1.7vh"
+    paragrafoSobre.style.fontSize = "1.7vh"
+    paragrafoImagens.style.fontSize = "1.7vh"
+} else if (screen.width > 320 && screen.width <=425) {
+    paragrafoInicio.style.fontSize = "2.5vh"
+    paragrafoSobre.style.fontSize = "2.5vh"
+    paragrafoImagens.style.fontSize = "2.5vh"
+} else if (screen.width > 425 && screen.width <=1024) {
+    paragrafoInicio.style.fontSize = "3.5vh"
+    paragrafoSobre.style.fontSize = "3.5vh"
+    paragrafoImagens.style.fontSize = "3.5vh"
+} else if (screen.width > 1024 ) {
+    paragrafoInicio.style.fontSize = "4vh"
+    paragrafoSobre.style.fontSize = "4vh"
+    paragrafoImagens.style.fontSize = "4vh"
 }
 
 setTimeout( selectSection(1) , 500)
